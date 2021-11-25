fb775d6911d8cf491a8455ed41c4f9393501065b
Phonker18 <tishembitov@yandex.ru>
2021-11-25 21:32:41 +0500

    Добавьте файлы проекта.

---
+++ T_Lab3_OOP/Storage.h
@@ -0,0 +1,170 @@
+#ifndef CSTORAGE_H
+#define CSTORAGE_H
+#include <cassert>
+
+template <class T> // это шаблон класса с T вместо фактического (передаваемого) типа данных
+class Storage 
+{
+private:
+    int m_length;
+    T** m_data;
+
+public:
+    Storage()
+    {
+        m_length = 0;
+        m_data = nullptr;
+    }
+
+    Storage(int length)
+    {
+        m_data = new T*[length];
+        m_length = length;
+    }
+
+    ~Storage()
+    {
+        delete[] m_data;
+    }
+
+    void setObject(T* object, int index) {
+        printf("Объект под индексом %d добавлен\n", index);
+        m_data[index] = object;
+    }
+
+    void Erase()
+    {
+        delete[] m_data;
+        // Присваиваем значение nullptr для m_data, чтобы на выходе не получить висячий указатель!
+        m_data = nullptr;
+        m_length = 0;
+    }
+
+
+    T& operator[](int index) 
+    {
+        assert(index >= 0 && index < m_length);
+        return *m_data[index];
+    }
+
+    // Функция reallocate() изменяет размер массива. Все существующие элементы внутри массива будут уничтожены. Процесс быстрый
+    void reallocate(int newLength)
+    {
+        // Удаляем все существующие элементы внутри массива
+        Erase();
+
+        // Если наш массив должен быть пустым, то выполняем возврат здесь
+        if (newLength <= 0)
+            return;
+
+        // Дальше нам нужно выделить новые элементы
+        m_data = new T*[newLength];
+        m_length = newLength;
+    }
+
+    // Функция resize() изменяет размер массива. Все существующие элементы сохраняются. Процесс медленный
+    void resize(int newLength)
+    {
+        // Если массив уже нужной длины, то выполняем return
+        if (newLength == m_length)
+            return;
+
+        // Если нужно сделать массив пустым, то делаем это и затем выполняем return
+        if (newLength <= 0)
+        {
+            Erase();
+            return;
+        }
+
+        // Теперь предположим, что newLength состоит, по крайней мере, из одного элемента. Выполняется следующий алгоритм действий:
+        // 1. Выделяем новый массив.
+        // 2. Копируем элементы из существующего массива в наш только что выделенный массив.
+        // 3. Уничтожаем старый массив и даем команду m_data указывать на новый массив.
+
+        // Выделяем новый массив
+        T** data = new T*[newLength];
+
+        // Затем нам нужно разобраться с количеством копируемых элементов в новый массив.
+        // Нам нужно скопировать столько элементов, сколько их есть в меньшем из массивов
+        if (m_length > 0)
+        {
+            int elementsToCopy = (newLength > m_length) ? m_length : newLength;
+
+            // Поочередно копируем элементы
+            for (int index = 0; index < elementsToCopy; ++index)
+                data[index] = m_data[index];
+        }
+
+        // Удаляем старый массив, так как он нам уже не нужен
+        delete[] m_data;
+
+        // И используем вместо старого массива новый! Обратите внимание, m_data указывает на тот же адрес, на который указывает наш новый динамически выделенный массив.
+        // Поскольку данные были динамически выделены, то они не будут уничтожены, когда выйдут из области видимости
+        m_data = data;
+        m_length = newLength;
+    }
+
+    void insertBefore(T* object, int index)
+    {
+        printf("Объект под индексом %d добавлен\n", index);
+        // Проверка корректности передаваемого индекса
+        assert(index >= 0 && index <= m_length);
+
+        // Создаем новый массив на один элемент больше старого массива
+        T** data = new T*[m_length + 1];
+
+        // Копируем все элементы аж до index
+        for (int before = 0; before < index; ++before)
+            data[before] = m_data[before];
+
+        // Вставляем наш новый элемент в наш новый массив
+        data[index] = object;
+
+        // Копируем все значения после вставляемого элемента
+        for (int after = index; after < m_length; ++after)
+            data[after + 1] = m_data[after];
+
+        // Удаляем старый массив и используем вместо него новый массив
+        delete[] m_data;
+        m_data = data;
+        ++m_length;
+    }
+
+    void remove(int index)
+    {
+        printf("Объект под индексом %d удален", index);
+        // Проверка на корректность передаваемого индекса
+        assert(index >= 0 && index < m_length);
+
+        // Если это последний элемент массива, то делаем массив пустым и выполняем return
+        if (m_length == 1)
+        {
+            Erase();
+            return;
+        }
+       
+        // Cоздаем новый массив на один элемент меньше нашего старого массива
+        T** data = new T*[m_length - 1];
+
+        // Копируем все элементы аж до index
+        for (int before = 0; before < index; ++before)
+            data[before] = m_data[before];
+
+        // Копируем все значения после удаляемого элемента
+        for (int after = index + 1; after < m_length; ++after)
+            data[after - 1] = m_data[after];
+
+        // Удаляем старый массив и используем вместо него новый массив
+        delete[] m_data;
+        m_data = data;
+        --m_length;
+    }
+
+    // Несколько дополнительных функций просто для удобства
+    void insertAtBeginning(T* object) { insertBefore(object, 0); }
+    void insertAtEnd(T* object) { insertBefore(object, m_length); }
+
+    int getLength()  { return m_length; }
+};
+
+#endif
---
+++ T_Lab3_OOP/T_Lab3_OOP.cpp
@@ -0,0 +1,79 @@
+﻿#include <iostream>
+#include "Storage.h"
+using namespace std;
+
+class Shape
+{
+public:
+    Shape() { };
+    virtual void show() 
+    {
+        cout << "Фигура " << endl;
+    }
+    ~Shape() 
+    {
+        cout << "Фигура удалилась" << endl;
+    }
+};
+class Point :public Shape 
+{
+private:
+    int x, y;
+public:
+    Point() 
+    {
+        x = 0;
+        y = 0;
+    }
+    virtual void show()  override
+    {
+        cout << "Точка" << endl;
+    }
+    ~Point() 
+    {
+        cout << "Точка удалилась" << endl;
+    }
+};
+class Section :public Shape 
+{
+private:
+    Point* p1, * p2;
+    int x1, x2, y1, y2;
+public:
+    Section() 
+    {
+        p1 = new Point();
+        p2 = new Point();
+        x1 = x2 = y1 = y2 = 0;
+    }
+    ~Section() 
+    {
+        cout << "Отрезок удалился" << endl;
+        delete p1;
+        delete p2;
+    }
+    virtual void show()  override
+    {
+        cout << "Отрезок" << endl;
+    }
+};
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+	Storage <Shape> storage(10);
+	// добавляем в него объекты
+	for (int i = 0; i < storage.getLength(); i++)
+		storage.setObject(new Point(), i);
+    Section *s = new Section();
+    storage.insertAtEnd(s);
+	// обращаемся поочередно ко всем
+    for (int i = 0; i < storage.getLength(); i++)
+    {
+        printf("объект под номером %d:", i);
+        storage.operator[](i).show();
+    }
+    printf("Количество объектов в хранилище до удаления:%d\n", storage.getLength());
+    storage.remove(5);
+    printf("\nКоличество объектов в хранилище после удаления:%d", storage.getLength());
+	return 0;
+}
